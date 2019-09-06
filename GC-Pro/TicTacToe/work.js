a7251ecfeae4747602f689af727bfdf0bde36a13
Achint <achintdawra740@gmail.com>
2019-09-06 13:17:45 +0530

    Add!

--- GC-Pro/TicTacToe/work.js
+++ GC-Pro/TicTacToe/work.js
@@ -1,9 +1,10 @@
 $("td").on("click",function(){
-    var str = $(this).text();
-    alert(str);
-    $(this).text("");
-    $(this).keypress(function(e){
-        // if(e.which === 13)
-           alert("Lock!");
+    $("input").keypress(function(e){
+        if(e.which === 88){
+             $(this).parent().text("X");
+            }
+        else{
+             $(this).parent().text("0");
+        }
     });
 });
