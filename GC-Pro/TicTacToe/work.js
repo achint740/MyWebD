0a10577497f6546c1aa194d063189d396e5a85e5
Achint <achintdawra740@gmail.com>
2019-09-12 18:15:13 +0530

    Added

--- GC-Pro/TicTacToe/work.js
+++ GC-Pro/TicTacToe/work.js
@@ -1,143 +1,61 @@
-function check(p){
-    for(var i=0;i<3;i++){
-    if(p[i][0] == 5 && p[i][1] == 5 && p[i][2] == 5)
-      return true;
-    if(p[0][i] == 5 && p[1][i] == 5 && p[2][i] == 5)
-      return true;
-    }
-    if(p[0][0] == 5 && p[1][1] == 5 && p[2][2] == 5)
-      return true;
-    if(p[0][2] == 5 && p[1][1] == 5 && p[2][0] == 5)
-      return true;
-    return false;
-}
-
-var p1 = new Array(3);
-for(var i=0;i<p1.length;i++){
-    p1[i] = new Array(3);
-}
-
-var p2 = new Array(3);
-for(var i=0;i<p1.length;i++){
-    p2[i] = new Array(3);
-}
-
-for(var i=0;i<3;i++){
-    for(var j=0;j<3;j++){
-        p1[i][j]=-1;
-        p2[i][j]=-1;
-    }
-}
-
-var turn = 0;
+var m=1;
 var gameOver=false;
-var p1win=false;
-var p2win=false;
-if(!gameOver){
-$("td").on("click",function(){
-    $("input").keypress(function(e){
-        if(e.which === 88){
-             $(this).parent().text("X");
-             var i=0;
-             var j=0;
-             if($(this).parent().is("#11")){
-                 i=0;j=0;
-             }
-             if($(this).parent().is("#12")){
-                i=0;j=1;
-            }
-            if($(this).parent().is("#13")){
-                i=0;j=2;
-            }
-            if($(this).parent().is("#21")){
-                 i=1;j=0;
-             }
-             if($(this).parent().is("#22")){
-                i=1;j=1;
-             }
-             if($(this).parent().is("#22")){
-                i=1;j=1;
-             }
-             if($(this).parent().is("#23")){
-                i=1;j=2;
-             }
-             if($(this).parent().is("#31")){
-                i=2;j=0;
-             }
-             if($(this).parent().is("#32")){
-                i=2;j=1;
-             }
-             if($(this).parent().is("#33")){
-                i=2;j=2;
-             }
-             if(turn==0){
-                 //Update p1
-                 p1[i][j]=5;
-             }
-             else{
-                 //Update p2
-                 p2[i][j]=5;
-             }
-         }
-        else{
-             $(this).parent().text("0");
-             var i=0;
-             var j=0;
-             if($(this).parent().is("#11")){
-                 i=0;j=0;
-             }
-             if($(this).parent().is("#12")){
-                i=0;j=1;
-            }
-            if($(this).parent().is("#13")){
-                i=0;j=2;
-            }
-            if($(this).parent().is("#21")){
-                 i=1;j=0;
-             }
-             if($(this).parent().is("#22")){
-                i=1;j=1;
-             }
-             if($(this).parent().is("#22")){
-                i=1;j=1;
-             }
-             if($(this).parent().is("#23")){
-                i=1;j=2;
-             }
-             if($(this).parent().is("#31")){
-                i=2;j=0;
-             }
-             if($(this).parent().is("#32")){
-                i=2;j=1;
-             }
-             if($(this).parent().is("#33")){
-                i=2;j=2;
-             }
-             if(turn==0){
-                //Update p1
-                p1[i][j]=5;
-             }
-             else{
-                //Update p2
-                p2[i][j]=5;
-             }
-         }
-        if(turn===0 && check(p1) == true){
-            gameOver=true;
-            p1win=true;
-        }
-        if(turn===1 && check(p2) == true){
-            gameOver=true;
-            p2win=true;
-        }
-        turn = 1 - turn;
-    });
+$("table tr td").click(function(){
+      if($(this).text() === "" && !gameOver){
+          if(m%2){
+              $(this).append("X");
+              $("#msg").text("P2 turn");
+          }
+          else{
+              $(this).append("O");
+              $("#msg").text("P1 turn");
+          }
+          m++;
+          var ans = winner();
+          if(ans!=-1 && ans!=""){
+              if(ans==="X"){
+                $("#WIN").text("Player 1 Won");
+              }
+              else{
+                $("#WIN").text("Player 2 Won");
+              }
+              gameOver=true;
+              $("#msg").text("GAME OVER!");
+              $("#msg").css('background-color',"blue");
+          }
+      }
 });
-}
 
-if(p1win == true){
-    $("h3").text("P1 won");
+function winner(){
+    var sp1 = $("#11").text();
+    var sp2 = $("#12").text();
+    var sp3 = $("#13").text();
+    var sp4 = $("#21").text();
+    var sp5 = $("#22").text();
+    var sp6 = $("#23").text();
+    var sp7 = $("#31").text();
+    var sp8 = $("#32").text();
+    var sp9 = $("#33").text();
+    //Rows
+    if((sp1==sp2) && (sp2==sp3))
+      return sp3;
+    else if((sp4==sp5) && (sp5==sp6))
+      return sp6;
+    else if((sp7==sp8) && (sp8==sp9))
+      return sp9;
+    //Columns
+    else if((sp1==sp4) && (sp4==sp7))
+      return sp7;
+    else if((sp2==sp5) && (sp5==sp8))
+      return sp8;
+    else if((sp3==sp6) && (sp6==sp9))
+      return sp9;
+    //Diagonals
+    else if((sp1==sp5) && (sp5==sp9))
+      return sp9;
+    else if((sp3==sp5) && (sp5==sp7))
+      return sp7;
+    else
+      return -1;
 }
-  
-
 
